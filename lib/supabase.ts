71643c93c5dec55d30fde2485ff91b0cb5e516b9
Ey <iyketheface@gmail.com>
2026-06-17 12:39:46 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ygqqwmjhhdgnhvjtnfjk.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseAnonKey = 'sb_publishable_qkoegnjQO-rGlFNbU6xchw_noLWt-uy'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
